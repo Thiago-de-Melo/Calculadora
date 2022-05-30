09ff4d2ca6e77d80c1d6c537708a190f4048a9d6
thiagodemelopachecos@gmail.com <thiagodemelopachecos@gmail.com>
2022-05-30 19:43:11 -0300

    Calculadora, exercício aula de desenvolvimento de sistemas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,86 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculadora';
+  
+  cont: number = 0;
+  valor_a: string = "";
+  valor_b: string = "";
+  valor_c: string = "";
+  valor: string = "";
+  valor1: number = 0;
+  calculo: string = "";
+  resultado: number = 0;
+
+ recebe_valor(event: any) {
+    if (this.cont < 8) {
+      this.cont++;
+      this.valor = this.valor + event.path[0].id;
+    }
+  }
+
+  somar() {
+    this.calculo = "+";
+    this.valor1 += Number(this.valor);      
+    this.valor = "";  
+  }
+
+  subtracao() {
+    this.calculo = "-";
+    this.valor1 = Number(this.valor);
+    this.valor = "";
+  }
+
+  porcentagem() {
+    this.calculo = "%";
+    this.valor1 = Number(this.valor);
+    this.valor = "";
+  }
+
+  raiz() {
+    this.valor1 = (Number(this.valor))**0.5;
+    this.valor = this.valor1.toString();
+  }
+
+  mutiplicar() {
+    this.calculo = "*";
+    this.valor1 = Number(this.valor);
+    this.valor = "";
+  }
+
+  dividir() {
+    this.calculo = "/";
+    this.valor1 = Number(this.valor);
+    this.valor = "";
+  }
+
+  igual() {
+    if (this.calculo === "+") {
+      this.resultado = this.valor1 + Number(this.valor);
+      this.valor = this.resultado.toString();
+    }
+    if (this.calculo === "-") {
+      this.resultado = this.valor1 - Number(this.valor);
+      this.valor = this.resultado.toString();
+    }
+    if (this.calculo === "*") {
+      this.resultado = this.valor1 * Number(this.valor);
+      this.valor = this.resultado.toString();
+    }
+    if (this.calculo === "/") {
+      this.resultado = this.valor1 / Number(this.valor);
+      this.valor = this.resultado.toString();
+    }
+    if (this.calculo === "%") {
+      this.resultado = Number(this.valor) - (Number(this.valor) * (this.valor1 / 100));
+      this.valor = this.resultado.toString();
+    }
+  }
+
+  limpa_campos() {
+    this.valor = "";
+    this.valor1 = 0;
+    this.cont = 0;
+    this.resultado;
+  }
+
 }
